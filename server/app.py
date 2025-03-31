d05d8f63c0bf81aea7ec4f7b81bc3dcb26c2cbf2
Edmund <edmundgachanja@gmail.com>
2025-03-31 13:11:43 +0300

    lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,21 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return make_response([art.to_dict() for art in articles], 201)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session["page_views"] = session.get("page_views") or 0
+    session["page_views"] += 1
 
-    pass
+    if session["page_views"] > 3 :
+        return make_response({"message": "Maximum pageview limit reached"}, 401)
+    
+    article = Article.query.filter_by(id=id).first()
+
+    return make_response(article.to_dict(), 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
